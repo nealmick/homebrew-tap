eeab40f73ff0a6e72f11dcf7c49126c73e163c6b
nealmick <nealmick99@gmail.com>
2024-10-24 11:27:49 -0400

    updates

--- ned.rb
+++ ned.rb
@@ -2,27 +2,20 @@ class Ned < Formula
   desc "Modern text editor built with ImGui"
   homepage "https://github.com/nealmick/ned"
   url "https://github.com/nealmick/ned/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "19c0ac7291264e5583f0058b13d36495d95b6c3443e24e8c7dd44c01881bdaeb"
+  sha256 "479df6f0d580cacf7c0994a93904ecd65dfdba1a55503320ed6398855fdee594"
   license "MIT"
 
   depends_on "cmake" => :build
   depends_on "glfw"
 
   def install
-    # Debug: Print current directory and list files
-    system "pwd"
-    system "ls", "-R"
-    
-    # Try to find imgui directory
-    system "find", ".", "-name", "imgui.cpp"
-    
-    ENV.deparallelize
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
-    
     bin.install "build/text_editor" => "ned"
-    prefix.install "fonts" if Dir.exist?("fonts")
-    prefix.install "icons" if Dir.exist?("icons")
+    
+    # Install additional resources
+    prefix.install "fonts"
+    prefix.install "icons"
   end
 
   test do
