e550253d63f8abfd3661f8c8071f514668eeba08
Neal <33704976+nealmick@users.noreply.github.com>
2024-10-30 12:02:14 -0400

    Update ned.rb

--- ned.rb
+++ ned.rb
@@ -1,25 +1,46 @@
 class Ned < Formula
-  desc "Modern text editor built with ImGui"
-  homepage "https://github.com/nealmick/ned"
-  url "https://github.com/nealmick/ned/releases/download/v1.1.0/ned-1.1.0.tar.gz"
-  sha256 "3e8bf25ec67a76d3bc66b262d0fb82ddb83387a240bacbaa6dff51bf5807edd5"
-  license "MIT"
-
-  depends_on "cmake" => :build
-  depends_on "glfw"
-
-  def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
-    bin.install "build/text_editor" => "ned"
-    
-    # Install additional resources
-    prefix.install "fonts"
-    prefix.install "icons"
-  end
-
-  test do
-    system "#{bin}/ned", "--version"
-  end
+ desc "Modern text editor built with ImGui"
+ homepage "https://github.com/nealmick/ned"
+ url "https://github.com/nealmick/ned/releases/download/v1.2.1/ned-1.2.1.tar.gz"
+ sha256 "e11cd9a195639828f5200156a5ca959f57bf052df0d8cdccafd097dc3ec74419"
+ license "MIT"
+ depends_on "cmake" => :build
+ depends_on "glfw"
+ def install
+   system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+   system "cmake", "--build", "build"
+   bin.install "build/text_editor" => "ned"
+   
+   # Install additional resources
+   prefix.install "fonts"
+   prefix.install "icons"
+   
+   # Create .app bundle with icon
+   (prefix/"Applications/Ned.app/Contents/MacOS").mkpath
+   ln_s bin/"ned", prefix/"Applications/Ned.app/Contents/MacOS/ned"
+   (prefix/"Applications/Ned.app/Contents/Resources").install "ned.icns"
+   (prefix/"Applications/Ned.app/Contents/Info.plist").write <<~EOS
+     <?xml version="1.0" encoding="UTF-8"?>
+     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+     <plist version="1.0">
+     <dict>
+       <key>CFBundleExecutable</key>
+       <string>ned</string>
+       <key>CFBundleIconFile</key>
+       <string>ned</string>
+       <key>CFBundleIdentifier</key>
+       <string>com.nealmick.ned</string>
+       <key>CFBundleName</key>
+       <string>Ned</string>
+       <key>CFBundlePackageType</key>
+       <string>APPL</string>
+       <key>CFBundleShortVersionString</key>
+       <string>#{version}</string>
+     </dict>
+     </plist>
+   EOS
+ end
+ test do
+   system "#{bin}/ned", "--version"
+ end
 end
-
